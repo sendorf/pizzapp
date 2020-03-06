402ee033abab85bee8b7a35b7405a9d32f4bfbf8
Sergio Diaz Alvarez <sendorf@gmail.com>
2020-03-06 00:33:57 +0000

    Create Stores controller

---
+++ app/controllers/stores_controller.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+class StoresController < ApplicationController
+  def create
+    @store = Store.new(store_params)
+    if @store.save
+      render json: @store, status: :ok
+    else
+      render json: @store.errors, status: :bad_request
+    end
+  rescue StandardError => e
+    render json: e, status: :unprocessable_entity
+  end
+
+  def update
+    @store = Store.find(params[:id])
+    if @store.update(store_params)
+      render json: @store, status: :ok
+    else
+      render json: @store.errors, status: :bad_request
+    end
+  rescue StandardError => e
+    render json: e, status: :unprocessable_entity
+  end
+
+  def show
+    render json: Store.find(params[:id]), status: :ok
+  rescue StandardError => e
+    render json: e, status: :unprocessable_entity
+  end
+
+  def index
+    render json: Store.all, status: :ok
+  rescue StandardError => e
+    render json: e, status: :unprocessable_entity
+  end
+
+  private
+
+  def store_params
+    params.require(:store).permit(:name, :address, :email, :phone)
+  end
+end
---
+++ spec/requests/stores_request_spec.rb
@@ -0,0 +1,154 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Stores', type: :request do
+  let(:store) { Store.new(name: name, address: address, email: email, phone: phone) }
+  let(:store_id) { 'hneknfdnks' }
+  let(:store_params) { { store: { ame: name, address: address, email: email, phone: phone } } }
+  let(:name) { 'store name' }
+  let(:address) { 'foo street' }
+  let(:email) { 'fake@foo.com' }
+  let(:phone) { '4567898765' }
+  let(:error_message) { 'foo error message' }
+
+  describe 'request list of all stores' do
+    context 'when there are no stores' do
+      it 'returns an empty collection' do
+        get stores_path
+        expect(response).to be_successful
+        expect(response.body).to eq [].to_json
+      end
+    end
+
+    context 'when there are stores' do
+      it 'returns the stores collection' do
+        expect(Store).to receive(:all).and_return [store]
+        get stores_path
+        expect(response).to be_successful
+        expect(response.body).to include(name)
+      end
+    end
+
+    context 'when there is an exception' do
+      it 'returns a 422 error' do
+        expect(Store).to receive(:all).and_raise StandardError.new(error_message)
+        get stores_path
+        expect(response).not_to be_successful
+        expect(response.code).to eq '422'
+        expect(response.body).to include(error_message)
+      end
+    end
+  end
+
+  describe 'request a store' do
+    context 'when there is no store' do
+      it 'returns an empty collection' do
+        get store_path(store_id)
+        expect(response).not_to be_successful
+        expect(response.body).to include("Couldn't find Store with 'id'=#{store_id}")
+      end
+    end
+
+    context 'when there is a store' do
+      it 'returns the store' do
+        expect(Store).to receive(:find).with(store_id).and_return store
+        get store_path(store_id)
+        expect(response).to be_successful
+        expect(response.body).to include('store name')
+      end
+    end
+
+    context 'when there is an exception' do
+      it 'returns a 422 error' do
+        expect(Store).to receive(:find).with(store_id).and_raise StandardError.new(error_message)
+        get store_path(store_id)
+        expect(response).not_to be_successful
+        expect(response.code).to eq '422'
+        expect(response.body).to include(error_message)
+      end
+    end
+  end
+
+  describe 'request creating a store' do
+    context 'when the request is correct' do
+      it 'returns the store' do
+        expect(Store).to receive(:new).and_return store
+        expect(store).to receive(:save).and_return true
+        post stores_path(store_params)
+        expect(response).to be_successful
+        expect(response.body).to include(name)
+        expect(response.body).to include(address)
+        expect(response.body).to include(email)
+        expect(response.body).to include(phone)
+      end
+    end
+
+    context 'when the request is not correct' do
+      it 'returns a 400 error' do
+        expect(Store).to receive(:new).and_return store
+        expect(store).to receive(:save).and_return false
+        expect(store).to receive(:errors).and_return error_message
+        post stores_path(store_params)
+        expect(response).not_to be_successful
+        expect(response.code).to eq '400'
+        expect(response.body).to include(error_message)
+      end
+    end
+
+    context 'when there is an exception' do
+      it 'returns a 422 error' do
+        expect(Store).to receive(:new).and_raise StandardError.new(error_message)
+        post stores_path(store_params)
+        expect(response).not_to be_successful
+        expect(response.code).to eq '422'
+        expect(response.body).to include(error_message)
+      end
+    end
+  end
+
+  describe 'request updating a store' do
+    context 'when the request is correct' do
+      it 'returns the store' do
+        expect(Store).to receive(:find).with(store_id).and_return store
+        expect(store).to receive(:update).and_return true
+        put store_path(store_id, store_params)
+        expect(response).to be_successful
+        expect(response.body).to include(name)
+        expect(response.body).to include(address)
+        expect(response.body).to include(email)
+        expect(response.body).to include(phone)
+      end
+    end
+
+    context 'when the request is not correct' do
+      it 'returns a 400 error' do
+        expect(Store).to receive(:find).with(store_id).and_return store
+        expect(store).to receive(:update).and_return false
+        expect(store).to receive(:errors).and_return error_message
+        put store_path(store_id, store_params)
+        expect(response).not_to be_successful
+        expect(response.code).to eq '400'
+        expect(response.body).to include(error_message)
+      end
+    end
+
+    context 'when there is an exception' do
+      it 'returns a 422 error' do
+        expect(Store).to receive(:find).with(store_id).and_raise StandardError.new(error_message)
+        put store_path(store_id, store_params)
+        expect(response).not_to be_successful
+        expect(response.code).to eq '422'
+        expect(response.body).to include(error_message)
+      end
+    end
+
+    context 'when there is no store' do
+      it 'returns an empty collection' do
+        put store_path(store_id, store_params)
+        expect(response).not_to be_successful
+        expect(response.body).to include("Couldn't find Store with 'id'=#{store_id}")
+      end
+    end
+  end
+end
